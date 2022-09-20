c7465d43b4ec06a3adb1b3d83c4636e533143ff5
equijas <equijas2@miners.utep.edu>
2022-09-19 20:40:35 -0600

    function created, being called

--- src/tokenizer.c
+++ src/tokenizer.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
+#include "tokenizer.h"
 
 int main() {
   char input[20];
   puts("->");
-  scanf("%s", input); 
+  scanf("%s", input);
 
+  space_char('d'); 
+}
+
+int space_char(char c) {
+  puts("1");
+  return 1; 
 }
