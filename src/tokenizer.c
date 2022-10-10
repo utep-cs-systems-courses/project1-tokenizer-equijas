dca9e294d103813553ae23e1d5afa546810f70ae
equijas <equijas2@miners.utep.edu>
2022-10-09 21:23:28 -0600

    refactored string read

--- src/tokenizer.c
+++ src/tokenizer.c
@@ -2,15 +2,6 @@
 #include "tokenizer.h"
 #include <stdlib.h>
 
-int main() {
-  char input[20];
-  printf("->");
-  fgets(input, 20, stdin);
-
-  int count = 2; 
-  printf("%d", count); 
-}
-
 int space_char(char c) {
   switch (c) {
   case '\t':
@@ -35,19 +26,19 @@ int space_char(char c) {
 int non_space_char(char c) {
   switch (c) {
   case '\t':
-    return 1; 
+    return 0; 
     break; 
   case ' ':
-    return 1;
+    return 0;
     break; 
   case '\n':
-    return 1;
+    return 0;
     break; 
   case '\0':
-    return 1;
+    return 0;
     break; 
   default:
-    return 0;
+    return 1;
     break; 
   } 
 }
@@ -67,12 +58,18 @@ char *word_terminator(char *word) {
 }
 
 int count_words(char *s) {
+  puts("hello!"); 
   int word_count = 0;
+ 
+
+
+  
   while (*s != '\0') {
     s = word_start(s);
     s = word_terminator(s);
     word_count++; 
   }
+  printf("%d", word_count); 
   return word_count; 
 }
 
--- src/uimain.c
+++ src/uimain.c
@@ -1,7 +1,16 @@
 #include <stdio.h>
+#include "tokenizer.h"
 
 int main() {
-  char input[30]; 
-  puts("->");
-  scanf("%s", input);
+
+  char input[30];
+  printf("Enter a sentence!\n>> ");
+  fgets(input, sizeof(input), stdin);  // read string
+  // printf("Input: ");
+  // puts(input);    // display string
+  return 0;
+  
+  // printf("%d", count_words(input));
+
+  return 0; 
 }
