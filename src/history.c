8877428cfb1f73429be655425a0caa69c02c491f
equijas <equijas2@miners.utep.edu>
2022-10-10 00:03:37 -0600

    history in progress

--- src/history.c
+++ src/history.c
@@ -1,1 +1,30 @@
-test
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "tokenizer.h"
+#include "history.h"
+
+
+List* init_history() {
+  List *history = (List*) malloc(sizeof(List)*100);
+  history->root = NULL;
+  return history; 
+}
+
+void add_history(List *list, char *str) {
+  Item *new_record = (Item*) malloc(sizeof(Item));
+  new_record->str = copy_str(str, strlen(str));
+  new_record->next = NULL;
+
+  if (list->root == NULL) {
+    list->root = new_record;
+    new_record->id = 1;
+  } else {
+    Item *head = list->root;
+    while(head->next != NULL) {
+      head = head->next;
+    }
+    new_record->id = (head->id) + 1;
+    head->next = new_record;
+  }
+}
--- src/uimain.c
+++ src/uimain.c
@@ -1,13 +1,21 @@
 #include <stdio.h>
+#include <string.h>
 #include "tokenizer.h"
+#include "history.h"
 
 int main() {
-
+  List *program_history =init_history(); 
   char input[100];
-  printf("Enter a sentence!\n>> ");
+  printf("\tEnter a sentence!\n\tEnter 'h' for history\n>> ");
   fgets(input, sizeof(input), stdin);  // read string
   //printf("Input: ");
   //puts(input);    // display string
+
+
+  if (strcmp(input, "h") == 0) {
+    add_history(program_history, input);
+    puts("inside"); 
+  }
   
   int test = count_words(input);
   printf("%d\n", test);
